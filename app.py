10bd731e5b8bfa0c1e71e93d57c21f5fa3863f41
Subham PRO <33375250+subhampro@users.noreply.github.com>
2024-12-08 07:27:40 +0000

    Update app.py to run on host '0.0.0.0' and port 5000

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def calculate():
         return "Invalid input. Please enter numeric values."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
